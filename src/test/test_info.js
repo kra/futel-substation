6c55f4215184ec47ca760b0fa40ba9c4ee319bf9
Karl Anderson <kra@monkey.org>
2017-03-22 11:05:59 -0700

    test

--- src/test/test_info.js
+++ src/test/test_info.js
@@ -67,32 +67,48 @@ describe('main', function() {
         });
     });
     describe('latest', function() {
-        it('should match whatever was populated in the metrics db', function(done) {
-            info.latest(
-                null,
-                function(result) {
-                    assert.ok(
-                        arrayCmp(
-                            result,
-                            [ 'latest channel events',
-                              '655 November 16, 2016 9:41 PM 911-9',
-                              '66.193.176.35 November 16, 2016 7:03 PM ring-r2d2',
-                              '668 November 16, 2016 5:10 PM macro-dial',
-                              'voipms November 16, 2016 3:18 PM incoming-dialstatus-CONGESTION',
-                              '670 November 16, 2016 1:52 PM outgoing-dialtone-wrapper',
-                              '680 November 16, 2016 10:35 AM outgoing-ivr',
-                              '702 November 15, 2016 11:49 PM wildcard-line-play',
-                              '667 November 15, 2016 11:34 PM outgoing-ivr',
-                              'callcentric November 15, 2016 10:06 AM ring-r2d2',
-                              'callcentric-r2d2 November 15, 2016 1:30 AM outgoing-dialstatus-CHANUNAVAIL',
-                              '690 November 14, 2016 4:20 PM wildcard-line',
-                              '681 November 13, 2016 12:34 PM outgoing-ivr',
-                              '656 October 30, 2016 5:48 PM outgoing-ivr',
-                              '691 October 14, 2016 6:12 PM outgoing-ivr',
-                              '650 October 10, 2016 5:42 PM outgoing-ivr',
-                              '704 October 9, 2016 12:04 AM outgoing-ivr' ]));
-                    done();
-                });
+        describe('all extensions', function() {
+            it('should match whatever was populated in the metrics db for all extensions', function(done) {
+                info.latest(
+                    null,
+                    function(result) {
+                        assert.ok(
+                            arrayCmp(
+                                result,
+                                [ 'latest channel events',
+                                  '655 November 16, 2016 9:41 PM 911-9',
+                                  '66.193.176.35 November 16, 2016 7:03 PM ring-r2d2',
+                                  '668 November 16, 2016 5:10 PM macro-dial',
+                                  'voipms November 16, 2016 3:18 PM incoming-dialstatus-CONGESTION',
+                                  '670 November 16, 2016 1:52 PM outgoing-dialtone-wrapper',
+                                  '680 November 16, 2016 10:35 AM outgoing-ivr',
+                                  '702 November 15, 2016 11:49 PM wildcard-line-play',
+                                  '667 November 15, 2016 11:34 PM outgoing-ivr',
+                                  'callcentric November 15, 2016 10:06 AM ring-r2d2',
+                                  'callcentric-r2d2 November 15, 2016 1:30 AM outgoing-dialstatus-CHANUNAVAIL',
+                                  '690 November 14, 2016 4:20 PM wildcard-line',
+                                  '681 November 13, 2016 12:34 PM outgoing-ivr',
+                                  '656 October 30, 2016 5:48 PM outgoing-ivr',
+                                  '691 October 14, 2016 6:12 PM outgoing-ivr',
+                                  '650 October 10, 2016 5:42 PM outgoing-ivr',
+                                  '704 October 9, 2016 12:04 AM outgoing-ivr' ]));
+                        done();
+                    });
+            });
+        });
+        describe('given extension', function() {
+            it('should match whatever was populated in the metrics db for given extensions', function(done) {
+                info.latest(
+                    '668',
+                    function(result) {
+                        assert.ok(
+                            arrayCmp(
+                                result,
+                                [ 'latest channel events',
+                                  '668 November 16, 2016 5:10 PM macro-dial' ]));
+                        done();
+                    });
+            });
         });
     });
 });
