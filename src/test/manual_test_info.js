2b71bd902a2cee8eb563ffe098aab62330ec92b0
Karl Anderson <kra@monkey.org>
2017-03-22 10:38:40 -0700

    manual test to mocha

--- src/test/manual_test_info.js
+++ src/test/manual_test_info.js
@@ -3,12 +3,6 @@ var dbFileName = 'test/assets/metrics.db'
 
 var info = new info_mod.Info(dbFileName);
 
-info.latest(
-    null,
-    function(result) {
-        result.map(function (line) { console.log(line); });
-    });
-
 info.stats(
     null,
     null,
