ccef41e5338eb8f8c83a70cd6d43a2d3bb42a88f
Karl Anderson <kra@monkey.org>
2017-10-04 16:56:48 -0700

    log more actions

--- src/test/manual_test_snspoller.js
+++ src/test/manual_test_snspoller.js
@@ -5,6 +5,8 @@ var config = require('../config');
 function Client() {}
 Client.prototype.noisySay = function(body) { console.log(body); };
 Client.prototype.peerStatusAction = function(peer, status) { console.log(peer); console.log(status); };
+Client.prototype.confBridgeJoinAction = function() { console.log('confBridgeJoinAction'); };
+Client.prototype.confBridgeLeaveAction = function() { console.log('confBridgeLeaveAction'); };
 var client = new Client();
 
 var poller = snspoller.Poller(
