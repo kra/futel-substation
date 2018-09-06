b0cce49cbc0685eb780727933340f2005a929a0c
Karl Anderson <kra@monkey.org>
2018-09-05 20:05:14 -0700

    foo

--- src/mechaoperator/config.js
+++ src/mechaoperator/config.js
@@ -1,11 +1,13 @@
 var config = {
-    channels: ["#cor", "#pdxbots", "#futel"],
+    // XXX testing
+    //channels: ["#cor", "#pdxbots", "#futel"],
+    channels: ["#xjwpo"],
     noisyChannels: ['#cor', '#pdxbots', "#futel"],
     server: "irc.freenode.net",
     botName: "mechaoperator",
     userName: "mechaoperator",
     realName: "Futel Mechanical Operator",
-    dbFileName: '/opt/futel/stats/prod/metrics.db',
+    dbFileName: '/opt/futel/var/spool/stats/prod/metrics.db',
     eventHostname: 'futel-prod.phu73l.net'
 };
 
