494fd178a4dbed5ad468bd87768e5b22af4c79b3
Karl Anderson <kra@monkey.org>
2021-04-21 18:48:54 -0700

    add stats tests

--- src/mechaoperator/test/test_info.js
+++ src/mechaoperator/test/test_info.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var sinon = require('sinon');
 var info_mod = require('../info');
 
+var many_days = 5000;           // enough days to get everything in the db
+
 var getInfo = function() {
     var info = new info_mod.Info('test/assets/metrics.db');    
     return info;
@@ -194,4 +196,34 @@ describe('main', function() {
             });
         });
     });
+
+    describe('stats', function() {
+        describe('all extensions', function() {
+            it('should match whatever was populated in the metrics db for all extensions', function(done) {
+                info.stats(
+                    many_days,
+                    null,
+                    function(result) {
+                        assert.deepEqual(
+                            result,
+                            ['outgoing-by-extension:2475 outgoing-ivr:2272 outgoing-dialtone-wrapper:1812 macro-dial:1368 macro-filterdial:1342 ring-r2d2:204 ring-xnor:107 ring-oskar:99 outgoing-dialstatus-tryagain:68 incoming-dialstatus-CHANUNAVAIL:68 delay-20:56 outgoing-dialstatus-CONGESTION:55 community-ivr:54 futel-information:32 911-9:31 incoming-dialstatus-CONGESTION:23 directory-ivr:23 delay-10:23 operator:19 wildcard-line:18 delay-5:18 incoming-ivr:16 outgoing-voicemail:14 internal-dialtone-wrapper:11 voicemail-main:10 services-ivr:9 ring-garth:9 incoming-dialstatus-BUSY:7 filter-outgoing-blocked:7 utilities-ivr:6 operator-nopickup:6 incoming-voicemail:5 futel-conf:5 cnet-portal:5 info-211:4 dream-survey:4 admin-auth:4 outgoing-dialstatus-CHANUNAVAIL:3 next-vm:3 current-time:3 apology-line:3 voicemail:2 mayor-vm:2 wildcard-line-play:1 voicemail-information:1 ring-oskar-in:1 ring-iprc:1 ring-demo:1 random-number:1 mojave-conference:1']);
+                        done();
+                    });
+            });
+        });
+        describe('given extension', function() {
+            it('should match whatever was populated in the metrics db for given extensions', function(done) {
+                info.stats(
+                    many_days,                    
+                    '655',
+                    function(result) {
+                        assert.deepEqual(
+                            result,
+                            ['outgoing-ivr:274 outgoing-by-extension:274 outgoing-dialtone-wrapper:74 macro-dial:53 macro-filterdial:48 community-ivr:7 outgoing-voicemail:6 911-9:6 internal-dialtone-wrapper:5 directory-ivr:5 futel-information:4 outgoing-dialstatus-tryagain:3 operator:3 wildcard-line:2 voicemail-main:2 utilities-ivr:2 current-time:2 services-ivr:1 outgoing-dialstatus-CONGESTION:1 outgoing-dialstatus-CHANUNAVAIL:1 next-vm:1 mayor-vm:1 info-211:1 dream-survey:1 apology-line:1']);
+                        done();
+                    });
+            });
+        });
+    });
+
 });
